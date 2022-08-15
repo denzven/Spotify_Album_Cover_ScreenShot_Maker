98cb8d44c502c3c008f4dc5055610ec5ebf21eeb
Denzven <denzvenvadakkan@gmail.com>
2022-08-15 21:38:03 +0530

    ..

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 window.onload = function () {
 
-    //Setting up Canvas
-    const canvas = document.getElementById('screenshotCanvas');
-    const ctx = canvas.getContext('2d');
+    // Adding Press enter to submit functionanlity
+    window.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("submit").click();
+        }
 
-    // All the Options
-    var songName       = document.getElementById("songName");
-    var artistName     = document.getElementById("artistName");
-    var playlistName   = document.getElementById("playlistName");
-    //var isLiked        = document.getElementById("isLiked");
-    //var isShuffle      = document.getElementById("isShuffle");
-    var isPAL          = document.getElementById("isPAL");
-    //var isPaused       = document.getElementById("isPaused");
-    var albumCover     = document.getElementById("albumCover");
-    var bgColor        = document.getElementById("bgColor")
-    var downloadCanvas = document.getElementById("downloadCanvas");
-    var watermark      = document.getElementById("watermark");
-    var chkstr         = 
-`_________________________________________
+        if (event.ctrlKey && event.key == 'p') {
+            event.preventDefault();
+            downloadCanvas.click();
+            console.log("crtle p")
+        }
+    });
+
+    // On clicking submit btn
+    document.getElementById('submit').addEventListener('click', () => {
+        //Setting up Canvas
+        var canvas = document.getElementById('screenshotCanvas');
+        var ctx = canvas.getContext('2d');
+
+        // All the Options
+        var songName = document.getElementById("songName");
+        var artistName = document.getElementById("artistName");
+        var playlistName = document.getElementById("playlistName");
+        var isPAL = document.getElementById("isPAL");
+        var albumCover = document.getElementById("albumCover");
+        var bgColor = document.getElementById("bgColor")
+        var downloadCanvas = document.getElementById("downloadCanvas");
+        var chkstr =
+            `_________________________________________
 | songName       : ${songName.value}
 | artistName     : ${artistName.value}
 | playlistName   : ${playlistName.value}
@@ -27,116 +39,97 @@ window.onload = function () {
 | downloadCanvas : ${downloadCanvas.value}
  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 `
-    // ------------------------------------------- //
-    //Coords List from JSON
-    var request = new XMLHttpRequest();
-    request.open("GET", "https://raw.githubusercontent.com/denzven/Spotify_Album_Cover_ScreenShot_Maker/main/assets/presets.json", false);
-    request.send(null)
-    var data = JSON.parse(request.responseText);
 
-    var preset = "default"
+        // ------------------------------------------- //
+        //Coords List from JSON
+        var request = new XMLHttpRequest();
+        request.open("GET", "https://raw.githubusercontent.com/denzven/Spotify_Album_Cover_ScreenShot_Maker/main/assets/presets.json", false);
+        request.send(null)
+        var data = JSON.parse(request.responseText);
 
-    //Images
-    var [UIx,UIy,UIw,UIh] = [data[preset]["UI"]["x"],data[preset]["UI"]["y"],data[preset]["UI"]["w"],data[preset]["UI"]["h"]]
-    var [albumCoverx,albumCovery,albumCoverw,albumCoverh] = [[data[preset]["albumCover"]["x"],data[preset]["albumCover"]["y"],data[preset]["albumCover"]["w"],data[preset]["albumCover"]["h"]]]
-    var [PALx,PALy,PALw,PALh] = [data[preset]["PAL"]["x"],data[preset]["PAL"]["y"],data[preset]["PAL"]["w"],data[preset]["PAL"]["h"]]
+        var preset = "default"
 
-    //Text
-    var [songNamex,songNamey,songNamec] = [data[preset]["songName"]["x"],data[preset]["songName"]["y"],data[preset]["songName"]["c"]]
-    var [artistNamex,artistNamey,artistNamec] = [data[preset]["artistName"]["x"],data[preset]["artistName"]["y"],data[preset]["artistName"]["c"]]
-    var [playlistNamex,playlistNamey,playlistNamec] = [data[preset]["playlistName"]["x"],data[preset]["playlistName"]["y"],data[preset]["playlistName"]["c"]]
+        //Images
+        var [UIx, UIy, UIw, UIh] = [data[preset]["UI"]["x"], data[preset]["UI"]["y"], data[preset]["UI"]["w"], data[preset]["UI"]["h"]]
+        var [albumCoverx, albumCovery, albumCoverw, albumCoverh] = [
+            [data[preset]["albumCover"]["x"], data[preset]["albumCover"]["y"], data[preset]["albumCover"]["w"], data[preset]["albumCover"]["h"]]
+        ]
+        var [PALx, PALy, PALw, PALh] = [data[preset]["PAL"]["x"], data[preset]["PAL"]["y"], data[preset]["PAL"]["w"], data[preset]["PAL"]["h"]]
 
-    // ------------------------------------------- //
+        //Text
+        var [songNamex, songNamey, songNamec] = [data[preset]["songName"]["x"], data[preset]["songName"]["y"], data[preset]["songName"]["c"]]
+        var [artistNamex, artistNamey, artistNamec] = [data[preset]["artistName"]["x"], data[preset]["artistName"]["y"], data[preset]["artistName"]["c"]]
+        var [playlistNamex, playlistNamey, playlistNamec] = [data[preset]["playlistName"]["x"], data[preset]["playlistName"]["y"], data[preset]["playlistName"]["c"]]
 
-    // Clipping Text
-    var songNameVal     = songName.value.length > songNamec ? songName.value.slice(0,songNamec) + "..." : songName.value;
-    var artistNameVal   = artistName.value.length > artistNamec ? artistName.value.slice(0,artistNamec) + "..." : artistName.value;
-    var playlistNameVal = playlistName.value.length > playlistNamec ? playlistName.value.slice(0,playlistNamec) + "..." : playlistName.value;    
+        // ------------------------------------------- //
 
-    //Download Canvas
-    downloadCanvas.addEventListener('click', () => {
-        downloadCanvas.href = (canvas.toDataURL('image/png', 1))
-    })
-
-    // Adding Press enter to submit functionanlity
-    window.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-          event.preventDefault();
-          document.getElementById("submit").click();
-        }
-
-        if (event.ctrlKey && event.key =='p') {
-          event.preventDefault();
-          downloadCanvas.click();
-          console.log("crtle p")
-        }
-      }); 
-
-    // On clicking submit btn
-    document.getElementById('submit').addEventListener('click',() => {
+        //Download Canvas
+        downloadCanvas.addEventListener('click', () => {
+            downloadCanvas.href = (canvas.toDataURL('image/png', 1))
+        })
+        // Clipping Text
+        var songNameVal = songName.value.length > songNamec ? songName.value.slice(0, songNamec) + "..." : songName.value;
+        var artistNameVal = artistName.value.length > artistNamec ? artistName.value.slice(0, artistNamec) + "..." : artistName.value;
+        var playlistNameVal = playlistName.value.length > playlistNamec ? playlistName.value.slice(0, playlistNamec) + "..." : playlistName.value;
 
         //infodump
         console.log(chkstr)
 
         //reset canvas
-        ctx.clearRect(0,0,canvas.width,canvas.height)
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
 
         // creating new image objects
-        //var likedImage      = new Image();
-        //var shuffleImage    = new Image();
-        var PALImage        = new Image();
-        //var pausedImage     = new Image();
+        var PALImage = new Image();
         var albumCoverImage = new Image();
         var UIImage = new Image();
 
         //fills bgColor
         ctx.fillStyle = bgColor.value;
-        ctx.fillRect(0,0,canvas.width,canvas.height);
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         //Loads ui elements
         UIImage.onload = () => {
-            ctx.drawImage(UIImage, UIx,UIy,UIw,UIh);
-        }; UIImage.src = "assets/ui/ui.png";
+            ctx.drawImage(UIImage, UIx, UIy, UIw, UIh);
+        };
+        UIImage.src = "assets/ui/ui.png";
+
+        //load album cover
+        try {
+            albumCoverImage.onload = () => {
+                ctx.drawImage(albumCoverImage, albumCoverx, albumCovery, albumCoverw, albumCoverh)
+            };
+            albumCoverImage.src = "assets/albumCovers/1.jpeg";
+        } catch (err) {
+            console.log(err)
+        }
 
         // Loading Fonts
         document.fonts.ready
-        .then(() => {
-            //adding playlistName
-            ctx.font = '12px gothammedium';
-            ctx.fillStyle = "white";
-            ctx.textAlign = "center";
-            ctx.fillText(playlistNameVal,playlistNamex,playlistNamey);
+            .then(() => {
+                //adding playlistName
+                ctx.font = '12px gothammedium';
+                ctx.fillStyle = "white";
+                ctx.textAlign = "center";
+                ctx.fillText(playlistNameVal, playlistNamex, playlistNamey);
 
-            //adding songName
-            ctx.font = '24px gothammedium';
-            ctx.fillStyle = "white";
-            ctx.textAlign = "start";
-            ctx.fillText(songNameVal,songNamex,songNamey);
+                //adding songName
+                ctx.font = '24px gothammedium';
+                ctx.fillStyle = "white";
+                ctx.textAlign = "start";
+                ctx.fillText(songNameVal, songNamex, songNamey);
 
-            //adding artistName
-            ctx.font = '16px gothamthin';
-            ctx.fillStyle = "white";
-            ctx.fillText(artistNameVal,artistNamex,artistNamey);
-        }).catch(() => {
-          console.log("Error");
-        });
+                //adding artistName
+                ctx.font = '16px gothamthin';
+                ctx.fillStyle = "white";
+                ctx.fillText(artistNameVal, artistNamex, artistNamey);
+            });
 
         // quick PAL check and loading album cover
-        try{
         if (isPAL.checked) {
-            albumCoverImage.onload = () =>{
-                ctx.drawImage(albumCoverImage,albumCoverx,albumCovery,albumCoverw,albumCoverh)
-                PALImage.onload = () => {
-                    ctx.drawImage(PALImage,PALx,PALy,PALw,PALh)
-                }; PALImage.src = "assets/ui/PAL.png"; 
-            }; albumCoverImage.src = URL.createObjectURL(albumCover.files[0]);
-        }else{
-            albumCoverImage.onload = () =>{
-                ctx.drawImage(albumCoverImage,albumCoverx,albumCovery,albumCoverw,albumCoverh)
-            }; albumCoverImage.src = URL.createObjectURL(albumCover.files[0]);
-        };
-        }catch(err){
-            console.log(err)
+            PALImage.onload = () => {
+                ctx.drawImage(PALImage, PALx, PALy, PALw, PALh)
+            };
+            PALImage.src = "assets/ui/PAL.png";
         }
     });
 }
